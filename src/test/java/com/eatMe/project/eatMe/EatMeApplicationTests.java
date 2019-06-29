cbb277bb5611998918f249ac3f067207657e2f17
dagmar <dagmararaczak@gmail.com>
2019-06-29 12:48:18 +0200

    first

---
+++ src/test/java/com/eatMe/project/eatMe/EatMeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eatMe.project.eatMe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EatMeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
