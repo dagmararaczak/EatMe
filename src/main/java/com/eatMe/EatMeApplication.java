19f52adf8a176061b938debc9e7d23f709fa0b08
dagmar <dagmararaczak@gmail.com>
2019-07-09 21:48:55 +0200

    update

--- src/main/java/com/eatMe/EatMeApplication.java
+++ src/main/java/com/eatMe/EatMeApplication.java
@@ -14,18 +14,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EatMeApplication implements CommandLineRunner {
 
-	@Autowired
-	IngredientsRepository ingredientsRepository;
-
-	@Autowired
-	RestaurantRepository restaurantRepository;
-	@Autowired
-	RestaurantService restaurantService;
-	@Autowired
-	MealRepository repository;
-
-	@Autowired
-	MenuRepository menuRepository;
 
 
 
